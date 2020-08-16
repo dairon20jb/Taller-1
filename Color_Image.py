4dba169d9e4b6035ae8a3a88413a8f04382c0c55
dairon20jb <dairon.barbosa@javeriana.edu.co>
2020-08-16 12:00:20 -0500

    Clase Persona
    Comentado

--- Color_Image.py
+++ Color_Image.py
@@ -2,44 +2,37 @@ import math
 import numpy as np
 import cv2
 import os
-class ColorImage:
-    def __init__(self,string):
-        self.string= string
-        image_name = 'lena.png'
-        self.carga_imagen= cv2.imread(os.path.join(string, image_name))
+class ColorImage: # Se crea la clase ColorImage
+    def __init__(self,string,image_name): #Constructor definición
+        self.string= string # String es la ruta de la imagen
+        self.image_name = image_name
+        self.carga_imagen= cv2.imread(os.path.join(self.string,self.image_name)) #Carga la imagen dada la dirección y nombre de la imagen
     def display_Properties(self):
-        cv2.imshow('Imagen', self.carga_imagen)
-        cv2.waitKey(0)
-        print("- Dimensiones de la imagen:")
-        print(self.carga_imagen.shape)
+        cv2.imshow('Imagen Original', self.carga_imagen) # Imagen original visualización
+        print("Las dimensiones de la imagen son:")
+        print(self.carga_imagen.shape[1], "pixeles de ancho por", self.carga_imagen.shape[0], "pixeles de alto" ) # Se imprimen las respectivas dimensiones de la imagen
     def make_Gray(self):
-        gris = cv2.cvtColor(self.carga_imagen, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('Imagen', gris)
-        cv2.waitKey(0)
+        gris = cv2.cvtColor(self.carga_imagen, cv2.COLOR_BGR2GRAY) #Imagen convertida a escala de grises
+        cv2.imshow('Imagen en grises', gris)
     def colorize_RGB(self,canal):
-        if canal == "rojo":
-            b=self.carga_imagen
+        if canal == "Rojo":  # Los canales Verde y Azul se convierten en cero si el canal de color seleccionado es Rojo
+            b=self.carga_imagen.copy()
             b[:, :, 0] = 0
-            b[:, :, 1] = 0
-            cv2.imshow('Imagen',b)
-            cv2.waitKey(0)
-        if canal == "verde":
-            b = self.carga_imagen
+            b[:, :, 1] = 0 
+            cv2.imshow('Imagen Rojiza',b)# Se visualiza imagen Rojiza
+        if canal == "Verde": # Los canales Rojo y Azul se convierten en cero si el canal de color seleccionado es Verde
+            b = self.carga_imagen.copy()
             b[:, :, 0] = 0
             b[:, :, 2] = 0
-            cv2.imshow('Imagen', b)
-            cv2.waitKey(0)
-        if canal == "azul":
-            b = self.carga_imagen
+            cv2.imshow('Verdoza', b) # Se visualiza imagen Verdoza
+        if canal == "Azul": # Los canales Verde y Rojo se convierten en cero si el canal de color seleccionado es Azul
+            b = self.carga_imagen.copy()
             b[:, :, 1] = 0
             b[:, :, 2] = 0
-            cv2.imshow('Imagen', b)
-            cv2.waitKey(0)
-
+            cv2.imshow('Imagen Azuloza', b)# Se visualiza imagen Azuloza
     def make_Hue(self):
-        hsv = cv2.cvtColor(self.carga_imagen, cv2.COLOR_BGR2HSV)
-        hsv[:,:,1] = 255
-        hsv[:,:,2] = 255
-        hsv = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-        cv2.imshow('Imagen', hsv)
-        cv2.waitKey(0)
+        hsv = cv2.cvtColor(self.carga_imagen, cv2.COLOR_BGR2HSV) # Imagen transformada al espacio HSV
+        hsv[:,:,1] = 255 # Componente S en 255
+        hsv[:,:,2] = 255 # Componente v en 255
+        hsv = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR) # Imagen transformada al espacio RGB de nuevo
+        cv2.imshow('Imagen Tonos Resaltados', hsv) # Se visualiza la imagen obtenida
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from  Color_Image import * #
-
+from  Color_Image import * # Importando
 if __name__ == '__main__': #
-    print("Digite la ruta de la carpeta")
-    direccion = 'C:\PRUEBA'
-    Mi_Instancia = ColorImage(direccion)
-    Mi_Instancia.display_Properties()
-    Mi_Instancia.colorize_RGB('verde')
-    Mi_Instancia.make_Gray()
-    Mi_Instancia.make_Hue()
-
+    direccion = input('Digite la ruta de la imagen: ') # Dirección de la carpeta en la que se encuentra la imagen
+    nombre_imagen = input('Digite el nombre de la imagen: ') # Nombre de la imagen a cargar, se debe incluir el formato (.png)
+    canal_color = input('Digite el canal de color deseado: ') #Elige entre Rojo, Verde y Azul
+    Mi_Instancia = ColorImage(direccion,nombre_imagen)#LLamado clase
+    Mi_Instancia.display_Properties()# Se visualiza la imagen original y se imprimen las respecivas dimensiones de esta
+    Mi_Instancia.colorize_RGB(canal_color) # Visualiza la imagen en el canal previamente indicado
+    Mi_Instancia.make_Gray() # Imagen escala de grises
+    Mi_Instancia.make_Hue() # Imagen tonos resaltados
+    cv2.waitKey(0)
